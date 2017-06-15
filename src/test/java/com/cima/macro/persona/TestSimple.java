ebf549deae03c5a39a29857e7599cbabc763c33b
Emilio Recio <emilio.recio>
2017-06-15 10:43:10 -0300

    Testç

--- src/test/java/com/cima/macro/persona/TestSimple.java
+++ src/test/java/com/cima/macro/persona/TestSimple.java
@@ -94,7 +94,7 @@ public class TestSimple {
         WebDriver driver = new ChromeDriver(cds, chromeOption);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         System.out.println("================================================================================");
-        System.out.println("==============================" + ReadConfigFile.getProperty(PATH_VNCDISPLAYPORT) + "==============================");
+        System.out.println("============================== " + ReadConfigFile.getProperty(PATH_VNCDISPLAYPORT) + " ==============================");
         System.out.println("================================================================================");
         return driver;
     }
